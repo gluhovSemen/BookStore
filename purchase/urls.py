1f90c1da4d530b146e648a45c907909d6fbf50b6
SemenGlukhov <rusglu02@mail.ru>
2023-05-15 09:49:41 +0200

    Purchase create and list views done

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from purchase.views import PurchaseList
+from purchase.views import CreatePurchaseAndClearCart, UserPurchaseListAPIView
 
 urlpatterns = [
-    path("purchases/", PurchaseList.as_view(), name="purchase-list"),
-    path("purchases/<int:cart_pk>/", PurchaseList.as_view(), name="purchase-list"),
+    path("purchases/", CreatePurchaseAndClearCart.as_view(), name="purchase-create"),
+    path("purchases/list/", UserPurchaseListAPIView.as_view(), name="purchase-list"),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,24 +1,56 @@
-from rest_framework import generics, permissions
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db import transaction
 
-from purchase.services import create_purchase_empty_cart
+from cart.models import Cart
 from purchase.models import Purchase
 from purchase.serializers import PurchaseSerializer
+from utils.permissions import IsOwner
 
 
-class PurchaseList(generics.ListCreateAPIView):
-    queryset = Purchase.objects.all()
+class CreatePurchaseAndClearCart(APIView):
+    permission_classes = [IsOwner]
+
+    @transaction.atomic
+    def post(self, request):
+        user = request.user
+        try:
+            cart = (
+                Cart.objects.select_related("customer")
+                .prefetch_related("cartitem_set")
+                .get(customer=user)
+            )
+        except Cart.DoesNotExist:
+            return Response({"message": "Cart does not exist"}, status=404)
+
+        # Create purchases for each cart item
+        purchases = []
+        for cart_item in cart.cartitem_set.all():
+            book = cart_item.book
+            if cart_item.quantity > book.available_stock:
+                raise ValidationError(
+                    {"message": f"Not enough stock available for book: {book.title}"}
+                )
+            purchase = Purchase(
+                customer=cart.customer,
+                book=book,
+                quantity=cart_item.quantity,
+                price=cart_item.price,
+            )
+            purchases.append(purchase)
+        Purchase.objects.bulk_create(purchases)
+
+        cart.cartitem_set.all().delete()
+
+        return Response({"message": "Purchases created and cart cleared"}, status=200)
+
+
+class UserPurchaseListAPIView(ListAPIView):
     serializer_class = PurchaseSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        create_purchase_empty_cart()
-        serializer.save(customer=self.request.user)
-
-
-class PurchaseDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Purchase.objects.all()
-    serializer_class = PurchaseSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsOwner]
 
     def get_queryset(self):
-        return Purchase.objects.filter(customer=self.request.user)
+        user = self.request.user
+        return Purchase.objects.filter(customer=user)
