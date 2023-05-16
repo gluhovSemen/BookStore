b8c1208787a54f756cea168ea8fa2dead26f33de
SemenGlukhov <rusglu02@mail.ru>
2023-05-16 14:08:20 +0200

    Cart views refactored

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,7 +1,10 @@
+from decimal import Decimal
+
 from rest_framework import serializers
 
+from book.models import Book
 from book.serializers import BookShortSerializer
-from cart.models import CartItem
+from cart.models import CartItem, Cart
 
 
 class CartItemSerializer(serializers.ModelSerializer):
@@ -12,15 +15,29 @@ class CartItemSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CartItemSerializerLong(serializers.ModelSerializer):
-    book_id = serializers.CharField(source="book.id")
-
-    class Meta:
-        model = CartItem
-        fields = ["id", "book_id", "quantity", "price"]
-
-
 class CartItemSerializerShort(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ["book", "quantity", "price"]
+
+
+class CartItemCreateSerializer(serializers.ModelSerializer):
+    """Serializer for creating a CartItem with the given book.id"""
+
+    book_id = serializers.IntegerField(write_only=True)
+    quantity = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "book_id", "quantity"]
+
+    def create(self, validated_data):
+        book_id = validated_data.get("book_id")
+        quantity = validated_data.get("quantity")
+        book = Book.objects.get(id=book_id)
+        cart = Cart.objects.get(customer=self.context["request"].user)
+        price = book.price * Decimal(quantity)
+        cart_item = CartItem.objects.create(
+            cart=cart, book=book, quantity=quantity, price=price
+        )
+        return cart_item
--- cart/views.py
+++ cart/views.py
@@ -2,9 +2,11 @@ from rest_framework import generics, status
 from rest_framework.generics import DestroyAPIView, CreateAPIView
 from rest_framework.response import Response
 
-from book.models import Book
-from cart.models import Cart, CartItem
-from cart.serializers import CartItemSerializer, CartItemSerializerLong
+from cart.models import CartItem
+from cart.serializers import (
+    CartItemSerializer,
+    CartItemCreateSerializer,
+)
 from utils.permissions import IsOwner
 
 
@@ -39,17 +41,12 @@ class CartItemCreateView(CreateAPIView):
     """Creates a CartItem with the given book.id"""
 
     permission_classes = [IsOwner]
-    serializer_class = CartItemSerializerLong
+    serializer_class = CartItemCreateSerializer
 
-    def post(self, request, *args, **kwargs):
-        book_id = request.data.get("book_id")
-        quantity = request.data.get("quantity")
-        book = Book.objects.get(id=book_id)
-        cart = Cart.objects.get(customer=request.user)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        cart_item = CartItem.objects.create(
-            cart=cart, book=book, quantity=quantity, price=book.price
-        )
+        cart_item = serializer.save()
 
-        serializer = CartItemSerializerLong(cart_item)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
